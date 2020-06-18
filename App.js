a6ee961092131339d8bbd33817f1882034ef9880
Hien Nguyen <toilati123vn@gmail.com>
2020-06-18 20:50:38 +0700

    Sample2

--- App.js
+++ App.js
@@ -1,37 +1,24 @@
-import React, { Component } from 'react';
-import { AppRegistry, View, Image, StyleSheet } from 'react-native';
 export const IMAGENAME = require('./background.jpg');
+import React, { Component } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 50,
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-  logo: {
-    width: 66,
-    height: 58,
-  },
+    stretch: {
+        width: 50,
+        height: 200,
+        resizeMode: 'stretch'
+    }
 });
 
-export default class DisplayAnImage extends Component {
-  render() {
-    return (
-        <View style={styles.container}>
-          <Image
-              style={styles.tinyLogo}
-              source={IMAGENAME}
-          />
-          <Image
-              style={styles.tinyLogo}
-              source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
-          />
-          <Image
-              style={styles.logo}
-              source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
-          />
-        </View>
-    );
-  }
+export default class DisplayAnImageWithStyle extends Component {
+    render() {
+        return (
+            <View>
+                <Image
+                    style={styles.stretch}
+                    source={IMAGENAME}
+                />
+            </View>
+        );
+    }
 }

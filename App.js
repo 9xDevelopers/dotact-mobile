95a10f1159df470b471795dc7a68f97756dbee7e
Hien Nguyen <toilati123vn@gmail.com>
2020-06-18 22:53:17 +0700

    FlatList Selectable

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import Constants from 'expo-constants';
 
 const DATA = [
@@ -17,21 +23,47 @@ const DATA = [
   },
 ];
 
-function Item({ title }) {
+function Item({ id, title, selected, onSelect }) {
   return (
-      <View style={styles.item}>
+      <TouchableOpacity
+          onPress={() => onSelect(id)}
+          style={[
+            styles.item,
+            { backgroundColor: selected ? '#6e3b6e' : '#f9c2ff' },
+          ]}
+      >
         <Text style={styles.title}>{title}</Text>
-      </View>
+      </TouchableOpacity>
   );
 }
 
 export default function App() {
+  const [selected, setSelected] = React.useState(new Map());
+
+  const onSelect = React.useCallback(
+      id => {
+        const newSelected = new Map(selected);
+        newSelected.set(id, !selected.get(id));
+
+        setSelected(newSelected);
+      },
+      [selected],
+  );
+
   return (
       <SafeAreaView style={styles.container}>
         <FlatList
             data={DATA}
-            renderItem={({ item }) => <Item title={item.title} />}
+            renderItem={({ item }) => (
+                <Item
+                    id={item.id}
+                    title={item.title}
+                    selected={!!selected.get(item.id)}
+                    onSelect={onSelect}
+                />
+            )}
             keyExtractor={item => item.id}
+            extraData={selected}
         />
       </SafeAreaView>
   );
